6ba129978c4d0f10f8fae8381c0733f6248357fa
imekachi <imekachi@gmail.com>
2021-11-04 22:22:52 +0700

    style: add import sorting, unused import removal lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'unused-imports'],
   extends: [
     'plugin:prettier/recommended',
     'plugin:@typescript-eslint/recommended',
@@ -9,8 +9,28 @@ module.exports = {
   rules: {
     // This rule is not compatible with Next.js's <Link /> components
     'jsx-a11y/anchor-is-valid': 'off',
-    // No unused variables
-    '@typescript-eslint/no-unused-vars': 'error',
     'arrow-body-style': ['error', 'as-needed'],
+    // Import sorting
+    'import/order': [
+      'warn',
+      {
+        pathGroups: [{ pattern: '@/**', group: 'internal' }],
+        alphabetize: { order: 'asc', caseInsensitive: true },
+        'newlines-between': 'never',
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+        ],
+      },
+    ],
+    'sort-imports': ['warn', { ignoreCase: true, ignoreDeclarationSort: true }],
+    // Remove unused imports
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
   },
 }
